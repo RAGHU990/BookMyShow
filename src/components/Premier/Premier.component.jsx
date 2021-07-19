4dcc6e87bae7a19ccac67d82c91d216aa41b7c11
Raghuram <raghu17122001@gmail.com>
2021-07-20 01:27:25 +0530

    added poster component

--- src/components/Premier/Premier.component.jsx
+++ src/components/Premier/Premier.component.jsx
@@ -94,9 +94,18 @@ export const Premier = () => {
 
    return  (
        <> 
+       <div className="flex flex-col items-start px-2"> 
+       <h3  className="text-white text-xl font-bold">
+           Premieres
+        </h3>
+       <p className="text-white text-sm ">
+           Brand new release every Friday
+        </p>
+       </div>
+       
        <Slider {...settings}>
        {PremierImages.map((image) => (
-           <Poster {...image}/>
+           <Poster {...image} isDark />
        ) )}
        </Slider>
         </>
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -5,13 +5,35 @@ import EntertainmentCardSlider from "../components/Entertainment/Entertainmentca
 import Premier from "../components/Premier/Premier.component";
 
 const Homepage = () => {
-    return <>  <div className="container mx-auto px-4">
+    return <> 
+    <div className="flex flex-col gap-10">
+      <div className="container mx-auto px-4 ">
+
         <h1 className="text-2xl font-bold text-gray-800 my-3">
             The Best of Entertainment
         </h1>
-    <EntertainmentCardSlider />
-    <Premier />
+        <EntertainmentCardSlider />
+     </div>
+
+     <div className="bg-bms-800 py-16">
+         
+
+        <div className="container mx-auto  px-4 flex flex-col gap-3 ">
+        <div className=" hidden  md:flex">
+             <div className="w-full h-full">
+                 <img src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png"
+                  alt="premier logo" 
+                  className="w-full h-full" />
+             </div>
+             
+         </div>
+        <Premier />
+        </div>
+    
+     </div>
+       
     </div>
+    
     </>
 };
 
